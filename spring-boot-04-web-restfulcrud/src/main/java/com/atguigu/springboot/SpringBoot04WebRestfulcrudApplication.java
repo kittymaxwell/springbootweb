48f2a6a3ab0646a9be062767096cde976340adc5
kittymaxwell <1303791589@qq.com>
2018-11-18 13:56:03 +0800

    打包方式

--- spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/SpringBoot04WebRestfulcrudApplication.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/SpringBoot04WebRestfulcrudApplication.java
@@ -1,7 +1,10 @@
 package com.atguigu.springboot;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
@@ -10,11 +13,17 @@ import java.util.Locale;
 
 
 @SpringBootApplication
-public class SpringBoot04WebRestfulcrudApplication {
+public class SpringBoot04WebRestfulcrudApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot04WebRestfulcrudApplication.class, args);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
 
 	@Bean
 	public ViewResolver myViewReolver(){
